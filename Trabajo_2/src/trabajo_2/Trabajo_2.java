8203b81c4f40ccd4560bfa0baf9fcf6005b2fad1
SoyNil <1422624@senati.pe>
2023-08-17 12:18:46 -0500

    Trabajo_Grupal

---
+++ Trabajo_2/src/trabajo_2/Trabajo_2.java
@@ -0,0 +1,13 @@
+package trabajo_2;
+import javax.swing.JOptionPane;
+public class Trabajo_2 {
+    public static void main(String[] args) {
+        int n1;
+        int n2;
+        int resultado;
+        n1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
+        n2=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número"));
+        
+    
+}
+}
